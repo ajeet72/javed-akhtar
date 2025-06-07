25115df8d220f8e241f7283869bc2325cd129036
Ajeet Kumar <ajeet62068bca@gmail.com>
2025-06-07 17:32:58 +0530

    projects images is added

--- src/components/projectsSection.tsx
+++ src/components/projectsSection.tsx
@@ -8,13 +8,18 @@ export default function ProjectsSection({ id }: { id: string }) {
       <ProjectCard
         title="Example Project"
         description="A web app for visualizing personalized Spotify data. View your top artists,visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more."
-        image="/project image.png"
+        image="/projectImage/beats.png"
       />
       <ProjectCard
         title="Example Project"
         description="A web app for visualizing personalized Spotify data. View your top artists,visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more."
-        image="/project image.png"
+        image="/projectImage/medium1.png"
         reverse={true}
+      />
+      <ProjectCard
+        title="Example Project"
+        description="A web app for visualizing personalized Spotify data. View your top artists,visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more."
+        image="/projectImage/pwskills.png"
       />
     </section>
   );
